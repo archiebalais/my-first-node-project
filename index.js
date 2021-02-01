5b5e8c6f644e8df837dbd19b8246be14d8ec46bf
archiebalais <archieabalais@gmail.com>
2021-02-01 10:50:50 -0800

    added other module

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const myModule = require('./myModule');
+const myOtherModule = require('./myOtherModule');
+
 console.log('hello world');
 
 function hello(name) {
@@ -6,3 +9,10 @@ function hello(name) {
 
 hello('Archie');
 hello('Amber');
+
+console.log(myModule.add(2, 2));
+console.log(myModule.subtract(8, 2));
+console.log(myModule.multiply(3, 3));
+
+// my other module
+myOtherModule.welcomeToNode()
---
+++ myModule.js
@@ -0,0 +1,13 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+const subtract = (num1, num2) => {
+    return num1 - num2;
+}
+
+const multiply = (num1, num2) => {
+    return num1 * num2;
+}
+
+module.exports = { add, subtract, multiply }
